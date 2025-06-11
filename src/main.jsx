07a88531360e39d1b30b5196a1a57bde1f18accc
Gabitronix1 <gabitronix1@outlook.com>
2025-06-11 12:10:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { WindsurfProvider } from '@windsurf/react-auth'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <WindsurfProvider>
